55095b69a0c163100746765f5442de48b5c2e2e0
Rafał Krasowski <rrafal.ka@gmail.com>
2023-05-29 13:00:00 +0200

    todo with add todo, show todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,55 @@
-
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("");
+  const [tasks, setTasks] = useState([])
 
+
+    function getId() {
+      if (!tasks.length) return 1;
+
+      return Math.max (...tasks.map((task) => task.id)) + 1
+
+    }
+  function handleInput(event) {
+    setValue(event.target.value);
+  }
+
+  function handleAddTask(event){
+    if (event.key === "Enter") {
+      setTasks([...tasks, {
+          id: getId(),
+          name: value,
+          status: "active"
+      }]);
+      setValue("");
+    }
+
+  }
+
+  function handeDelete(event) {
+
+  }
   return (
     <>
+      <h1>todos</h1>
+      <input
+          type="text"
+          value={value}
+          onChange={handleInput}
+          onKeyUp={handleAddTask}
+      />
+      <ul>
+        {tasks.map((task) => (
+            <li key={task.id}>
+            <span>{task.name}</span>
+                <button
+                onClick={handleDelete}
+                >delete
+                </button>
+            </li>
+        ))}
+      </ul>
 
     </>
   )
